e3aa9cd324d6a9b47144cdbd9d4cbc8fb5a3d2ce
Ape-xCV <ape-xcv@outlook.com>
2024-06-22 15:16:00 +0100

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+file=$(sort --random-sort www.txt)
+url=$(head -n 1 <<< "$file")
+url2=$(head -n 2 <<< "$file" | tail -n 1)
+echo $url
+echo $url2
+title=$(wget -qO- $url | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
+title=${title/&*;/}
+title2=$(wget -qO- $url2 | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
+title2=${title2/&*;/}
+echo -e '\033]2;'${title//-}'\007'
+
+me=$(basename "$0")
+target="${me%.sh}"
+
+sudo /root/$target "${title2//-}"
