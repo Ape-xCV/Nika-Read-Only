63fb9ccf53908a0382b95b695a2cdf20cb6f4b7e
Ape-xCV <ape-xcv@outlook.com>
2024-07-09 19:55:45 +0100

    Update Resolver.hpp

--- Resolver.hpp
+++ Resolver.hpp
@@ -4,9 +4,9 @@ struct Resolver {
     static QAngle CalculateAngle(Vector3D from, Vector3D to) {
         Vector3D newDirection = to.Subtract(from);
         newDirection.Normalize();
-        
-        float pitch = -asinf(newDirection.z) * (180 / M_PI);
-        float yaw = atan2f(newDirection.y, newDirection.x) * (180 / M_PI);
+
+        float pitch = -asinf(newDirection.z) * (180.0f / M_PI);
+        float yaw = atan2f(newDirection.y, newDirection.x) * (180.0f / M_PI);
 
         return QAngle(pitch, yaw);
     }
@@ -47,22 +47,22 @@ struct Resolver {
     static bool CalculateAimRotationNew(Vector3D start, Vector3D targetPosition, Vector3D targetVelocity, float bulletSpeed, float bulletScale, int steps, QAngle& result) {
         const float gravity = 750.0f / bulletScale;
 
-        float angle = 0;
-        float time = 0.0;
+        float angle = 0.0f;
+        float time = 0.0f;
         float timeStep = 1.0f / steps;
 
         for(int i = 0; i < steps; i++) {
             Vector3D predictedPosition = GetTargetPosition(targetPosition, targetVelocity, time);
             if (!OptimalAngle(start, predictedPosition, bulletSpeed, gravity, angle))
                 continue;
-            
+
             Vector3D direction = predictedPosition.Subtract(start);
             float horizontalDistance = direction.Magnitude2D();
             float travelTime = horizontalDistance / (bulletSpeed * cosf(angle));
 
             if(travelTime <= time){
-                result.x = -angle  * (180 / M_PI);
-                result.y = atan2f(direction.y, direction.x)  * (180 / M_PI);
+                result.x = -angle * (180.0f / M_PI);
+                result.y = atan2f(direction.y, direction.x) * (180.0f / M_PI);
                 return true;
             }
             time += timeStep;
@@ -82,9 +82,9 @@ struct Resolver {
     static bool OptimalAngle(const Vector3D start, const Vector3D end, const float speed, const float gravity, float& angle) {
         float horizontalDistance = start.Distance2D(end);
         float heightDifference = end.z - start.z;
-        
+
         float root = powf(speed, 4) - gravity * (gravity * powf(horizontalDistance, 2) + 2 * heightDifference * powf(speed, 2));
-        if (root < 0.0)
+        if (root < 0.0f)
             return false;
 
         float term1 = powf(speed, 2) - sqrt(root);
