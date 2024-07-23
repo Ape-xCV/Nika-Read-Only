c99db5a38e567dc9ac2d8cb787ddcc1693143b73
Ape-xCV <ape-xcv@outlook.com>
2024-07-23 04:15:07 +0100

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -108,8 +108,8 @@ struct Player {
         name = mem::ReadString(base + OFF_NAME, 1024, "Player name");
         teamNumber = mem::Read<int>(base + OFF_TEAM_NUMBER, "Player teamNumber");
         if (!isPlayer() && !isDummie()) { reset(); return; }
-        plyrDataTable = mem::Read<int>(base + OFF_NAMEINDEX, "Player Data Table");
-        if (cl->FEATURE_SPECTATORS_ON) { //_add
+        if (isPlayer()) { //_add
+            plyrDataTable = mem::Read<int>(base + OFF_NAMEINDEX, "Player Data Table");
             spectators = mem::Read<uint64_t>(OFF_REGION + OFF_SPECTATOR_LIST, "spectators");
             spctrIndex = mem::Read<int>(spectators + plyrDataTable * 8 + OFF_SPECTATOR_LIST_AUX, "Spectator Index");
             spctrBase =  mem::Read<uint64_t>(OFF_REGION + OFF_ENTITY_LIST + ((spctrIndex & 0xFFFF) << 5), "Spectator Base");
