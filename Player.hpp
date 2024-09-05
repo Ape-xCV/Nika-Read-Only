375b176818efb33d837d10fd5f8ae08b2be36080
Ape-xCV <ape-xcv@outlook.com>
2024-09-05 15:04:55 +0100

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -107,7 +107,7 @@ struct Player {
         if (lp->isValid()) {
             isLocal = base == lp->base;
             isFriendly = isSameTeam(map, lp);
-            isEnemy = !isFriendly || isDrone;
+            isEnemy = !isFriendly || map->isTrainingArea && isDrone;
             distanceToLocalPlayer = lp->localOrigin.Distance(localOrigin);
             distance2DToLocalPlayer = lp->localOrigin.Distance2D(localOrigin);
             //distance2DToLocalPlayer = lp->localOrigin.To2D().Distance(localOrigin.To2D());
