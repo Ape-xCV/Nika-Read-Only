1b18bbee8a78c7cfb02dc10f582e011e2aa4cc66
Ape-xCV <ape-xcv@outlook.com>
2026-01-23 03:47:12 +0000

    Add files via upload

--- virtio.cmd
+++ virtio.cmd
@@ -10,7 +10,10 @@ copy /Y "D:\NetKVM\w10\amd64\*.*" "."
 
 for /F "usebackq" %%a in (`PowerShell ^(Get-Date^).ToString^('dd'^)`) do set DAY=%%a
 for /F "usebackq" %%a in (`PowerShell ^(Get-Date^).ToString^('MM'^)`) do set MONTH=%%a
-set /A DEV=(%DAY% + %MONTH%) * 100 + %DAY% * %MONTH%
+set /A DEV=65535 - ((%DAY% + %MONTH%) * 100 + %DAY% * %MONTH%)
+echo %DEV%
+cmd /C exit %DEV%
+set DEV=%=EXITCODE:~-4%
 echo %DEV%
 
 powershell -Command "(Get-Content netkvm.inf).Replace('1AF4&DEV_1000&SUBSYS_00011AF4&REV_00, PCI\VEN_1AF4&DEV_1000', '8086&DEV_%DEV%&SUBSYS_00018086&REV_00, PCI\VEN_8086&DEV_%DEV%') | Out-File -encoding Ascii netkvm.txt"
