7ae0863fda3cc6d6e85f3c82a849f6bd74674ee8
Ape-xCV <ape-xcv@outlook.com>
2026-01-25 01:00:02 +0000

    Add files via upload

--- virtio.cmd
+++ virtio.cmd
@@ -6,8 +6,6 @@ if "%1"=="" (
 )
 cd %1
 
-copy /Y "D:\NetKVM\w10\amd64\*.*" "."
-
 for /F "usebackq" %%a in (`PowerShell ^(Get-Date^).ToString^('dd'^)`) do set DAY=%%a
 for /F "usebackq" %%a in (`PowerShell ^(Get-Date^).ToString^('MM'^)`) do set MONTH=%%a
 set /A DEV=49152 + (%DAY% + %MONTH%) * 100 + %DAY% * %MONTH%
@@ -16,10 +14,13 @@ cmd /C exit %DEV%
 set DEV=%=EXITCODE:~-4%
 echo %DEV%
 
-powershell -Command "(Get-Content netkvm.inf).Replace('1AF4&DEV_1000&SUBSYS_00011AF4&REV_00, PCI\VEN_1AF4&DEV_1000', '8086&DEV_%DEV%&SUBSYS_00018086&REV_00, PCI\VEN_8086&DEV_%DEV%') | Out-File -encoding Ascii netkvm.txt"
-fc netkvm.inf netkvm.txt
-powershell -Command "(Get-Content netkvm.txt).Replace('NetKVM', 'Intel').Replace('Red Hat, Inc.', 'Intel').Replace('Red Hat VirtIO', 'Intel(R)') | Out-File -encoding Ascii netkvm.inf"
-fc netkvm.txt netkvm.inf
+copy /Y "D:\NetKVM\w10\amd64\netkvm.*" "%DEV%.*"
+copy /Y "D:\NetKVM\w10\amd64\netkvmp.exe" "%DEV%.exe"
+
+powershell -Command "(Get-Content %DEV%.inf).Replace('1AF4&DEV_1000&SUBSYS_00011AF4&REV_00, PCI\VEN_1AF4&DEV_1000', '8086&DEV_%DEV%&SUBSYS_00018086&REV_00, PCI\VEN_8086&DEV_%DEV%').Replace('kvmnet6', 'oemnet').Replace('netkvmp', '%DEV%') | Out-File -encoding Ascii %DEV%.txt"
+fc %DEV%.inf %DEV%.txt
+powershell -Command "(Get-Content %DEV%.txt).Replace('netkvm', '%DEV%').Replace('NetKVM', 'Intel').Replace('Red Hat, Inc.', 'Intel').Replace('Red Hat VirtIO', 'Intel(R)') | Out-File -encoding Ascii %DEV%.inf"
+fc %DEV%.txt %DEV%.inf
 
 echo bcdedit /set testsigning on
 bcdedit /set testsigning on
