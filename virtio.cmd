de1acb0de6a73635c9a726dcedd172feecc1db1b
Ape-xCV <ape-xcv@outlook.com>
2026-01-25 01:39:55 +0000

    Add files via upload

--- virtio.cmd
+++ virtio.cmd
@@ -17,7 +17,7 @@ echo %DEV%
 copy /Y "D:\NetKVM\w10\amd64\netkvm.*" "%DEV%.*"
 copy /Y "D:\NetKVM\w10\amd64\netkvmp.exe" "%DEV%.exe"
 
-powershell -Command "(Get-Content %DEV%.inf).Replace('1AF4&DEV_1000&SUBSYS_00011AF4&REV_00, PCI\VEN_1AF4&DEV_1000', '8086&DEV_%DEV%&SUBSYS_00018086&REV_00, PCI\VEN_8086&DEV_%DEV%').Replace('kvmnet6', 'oemnet').Replace('netkvmp', '%DEV%') | Out-File -encoding Ascii %DEV%.txt"
+powershell -Command "(Get-Content %DEV%.inf).Replace('1AF4&DEV_1000&SUBSYS_00011AF4&REV_00, PCI\VEN_1AF4&DEV_1000', '8086&DEV_%DEV%&SUBSYS_00018086&REV_00, PCI\VEN_8086&DEV_%DEV%').Replace('kvmnet6', '%DEV%').Replace('netkvmp', '%DEV%') | Out-File -encoding Ascii %DEV%.txt"
 fc %DEV%.inf %DEV%.txt
 powershell -Command "(Get-Content %DEV%.txt).Replace('netkvm', '%DEV%').Replace('NetKVM', 'Intel').Replace('Red Hat, Inc.', 'Intel').Replace('Red Hat VirtIO', 'Intel(R)') | Out-File -encoding Ascii %DEV%.inf"
 fc %DEV%.txt %DEV%.inf
