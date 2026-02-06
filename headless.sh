c59195cad150f3f9eda5d9c84f95e19d1bdb44e2
Ape-xCV <ape-xcv@outlook.com>
2026-02-06 21:29:36 +0000

    Add files via upload

---
+++ headless.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ $(tty | grep /dev/tty1) ]; then
+  #Xvfb :1 -screen 0 1024x768x16 &
+  Xorg :1 -config dummy.conf -noreset +extension GLX +extension RANDR +extension RENDER &
+  sleep 3
+  x11vnc -display :1 -rfbport 5900 -forever &
+  sleep 3
+
+  export DISPLAY=:1
+  startlxde
+fi
