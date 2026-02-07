c4bcfb1a2e4766ec1f06f8371b80c7a4a623793b
Ape-xCV <ape-xcv@outlook.com>
2026-02-07 02:00:56 +0000

    Update autologin.sh

--- autologin.sh
+++ autologin.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" != 0 ]; then
-    faillock --reset
-    sudo -E "$0" "$USER"
-    exit $?
+if [[ "$EUID" != "0" ]]; then
+  faillock --reset
+  sudo -E "$0" $USER
+  exit $?
 fi
 
-if [ "$1" == "" ]; then exit 0; fi
+if [[ "$1" == "" ]]; then exit 0; fi
 file="/etc/systemd/system/getty@tty1.service.d/autologin.conf"
 mkdir -p "/etc/systemd/system/getty@tty1.service.d"
 echo "[Service]" > "$file"
 echo "ExecStart=" >> "$file"
-echo "ExecStart=/sbin/mingetty tty1 --autologin "$1" --noclear" >> "$file"
+echo "ExecStart=/sbin/mingetty tty1 --autologin $1 --noclear" >> "$file"
 echo $file
 systemctl enable getty@tty1.service
