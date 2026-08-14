805fce0ba9c4dbc56d24802499b1b294300aa7d0
Ape-xCV <ape-xcv@outlook.com>
2026-08-14 20:28:57 +0100

    Add files via upload

--- fedk2patch.sh
+++ fedk2patch.sh
@@ -511,5 +511,5 @@ virt-fw-vars --input "$VARS_DEST" --output "$VARS_DEST_2" \
   --set-false SecureBootEnable \
   --set-json "$DEFAULTS_JSON"
 
-cp -f "/usr/share/qemu/firmware/30-edk2-ovmf-4m-qcow2-x64-sb-enrolled.json" "/usr/share/qemu/firmware/10-edk2-ovmf-4m-qcow2-x64-fedk.json"
+cp -f /usr/share/qemu/firmware/?0-edk2-ovmf-4m-qcow2-x64-sb-enrolled.json "/usr/share/qemu/firmware/10-edk2-ovmf-4m-qcow2-x64-fedk.json"
 sed -i "/usr/share/qemu/firmware/10-edk2-ovmf-4m-qcow2-x64-fedk.json" -Ee "s/.secboot./.patched./"
